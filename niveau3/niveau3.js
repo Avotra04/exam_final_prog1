63692f7613e411ecffb04ef49648a5e4d3dae2d5
Tsilavina <hei.tsilavina.4@gmail.com>
2025-05-09 15:27:03 +0300

    done level 3

---
+++ niveau3/niveau3.js
@@ -0,0 +1,22 @@
+let nord = "NORD";
+let sud = "SUD";
+let est = "EAST";
+let ouest = "WEST"; 
+
+function reduceDirections(directions) {
+    for (let i = 0; i < directions.length;  ) {
+        if ((directions[i] == nord && directions[i+1]==sud) || (directions[i] == sud && directions[i+1]==nord)){
+            directions.splice(i,2)
+            if (i>0) {
+                i--
+            }
+        }else if ((directions[i] == est && directions[i+1] == ouest )|| (directions[i] == ouest && directions[i+1] == est) ){
+            directions.splice(i,2)
+            if (i>0) {
+                i--
+            }
+        } else {
+            i++
+        }
+    } return directions
+} 
