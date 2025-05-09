90cff4ef80ba89fea6c7fbfa8344862c9c391b77
NambininarivoTsila <hei.tsilavina.4@gmail.com>
2025-05-09 15:51:43 +0300

    fix niveau3.js

--- niveau3/niveau3.js
+++ niveau3/niveau3.js
@@ -3,14 +3,14 @@ let sud = "SUD";
 let est = "EAST";
 let ouest = "WEST"; 
 
-function reduceDirections(directions) {
-    for (let i = 0; i < directions.length;  ) {
+export function reduceDirections(directions) {
+    for (let i = 0; i < directions.length;) {
         if ((directions[i] == nord && directions[i+1]==sud) || (directions[i] == sud && directions[i+1]==nord)){
             directions.splice(i,2)
             if (i>0) {
                 i--
             }
-        }else if ((directions[i] == est && directions[i+1] == ouest )|| (directions[i] == ouest && directions[i+1] == est) ){
+        } else if ((directions[i] == est && directions[i+1] == ouest) || (directions[i] == ouest && directions[i+1] == est)){
             directions.splice(i,2)
             if (i>0) {
                 i--
